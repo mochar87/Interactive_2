f5fcfed19222b6fcabf3e81921a5fad2e5420c6c
mochar87 <acameron@cca.edu>
2016-12-06 14:16:02 -0800

    Edits

--- project3/js/script.js
+++ project3/js/script.js
@@ -23,8 +23,55 @@ $(document).ready(function(){
 		$redValue = Math.floor(Math.random()*255);
 		$greenValue = Math.floor(Math.random()*255);
 		$blueValue = Math.floor(Math.random()*255);	
-		$('.fill').css(
-			'background', 'rgba('+$redValue+','+$blueValue+','+$greenValue+', 1)'
+		$('#about').css(
+			'color', 'rgba('+$redValue+','+$blueValue+','+$greenValue+', 1)'
+		);
+		$('#yellow-e').css(
+			//'background-color', 'rgba('+$redValue/2+', 255, '+$greenValue+', 1)'
+			'background-color', 'rgba('+$redValue+','+$blueValue+','+$greenValue+', 1)'
+		);
+		
+
+		$('#violet-b').css(
+			//'background-color', 'rgba('+$redValue/2+', 255, '+$greenValue+', 1)'
+			'background-color', 'rgba('+$redValue+','+$blueValue+','+$greenValue+', 1)'
+
+		);
+
+		$('#red-c').css(
+			//'background-color', 'rgba('+$redValue/2+', 255, '+$greenValue+', 1)'
+			'background-color', 'rgba('+$redValue+','+$blueValue+','+$greenValue+', 1)'
+
+		);		
+
+		$('#indigo-a').css(
+			//'background-color', 'rgba('+$redValue/2+', 255, '+$greenValue+', 1)'
+			'background-color', 'rgba('+$redValue+','+$blueValue+','+$greenValue+', 1)'
+
+		);
+
+		$('#green-f').css(
+			//'background-color', 'rgba('+$redValue/2+', 255, '+$greenValue+', 1)'
+			'background-color', 'rgba('+$redValue+','+$blueValue+','+$greenValue+', 1)'
+
+		);
+
+		$('#blue-g').css(
+			//'background-color', 'rgba('+$redValue/2+', 255, '+$greenValue+', 1)'
+			'background-color', 'rgba('+$redValue+','+$blueValue+','+$greenValue+', 1)'
+
+		);
+
+		$('#orange-d').css(
+			//'background-color', 'rgba('+$redValue/2+', 255, '+$greenValue+', 1)'
+			'background-color', 'rgba('+$redValue+','+$blueValue+','+$greenValue+', 1)'
+
+		);
+
+		$('#glow').css(
+			//'background-color', 'rgba('+$redValue/2+', 255, '+$greenValue+', 1)'
+			'background-color', 'rgba('+$redValue+','+$blueValue+','+$greenValue+', 1)'
+
 		);
 	},500);
 	
